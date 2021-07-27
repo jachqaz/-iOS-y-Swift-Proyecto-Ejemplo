4603afeb7fab9fc7f2c1afdcb453625c881d46e3
Johan Castano <jcastano@vovsolutions.com>
2021-07-27 09:51:21 -0500

    Conexión de UI con el código Swift
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiqueta: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        etiqueta.text = "Ya Cambie"
     }
 
+    @IBAction func cambiarTexto(_ sender: Any) {
+        etiqueta.text = "Hola Mundo Cruel"
+    }
 
 }
 
