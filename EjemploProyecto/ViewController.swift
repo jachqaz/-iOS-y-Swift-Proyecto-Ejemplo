5fb4e0ac5f07dd04d7791fd0b854efda3bdc9cee
Johan Castano <jcastano@vovsolutions.com>
2021-07-27 11:57:48 -0500

    Buttons (Botones)
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -9,20 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var etiqueta: UILabel!
+//    @IBOutlet weak var etiqueta: UILabel!
 
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        etiqueta.text = "Ya Cambie"
-        etiqueta.text = "Ya Cambie por segunda vez"
-        etiqueta.text = "Ya Cambie  por tercera vez"
-        etiqueta.text = "Ya Cambie por cuarta vez"
+//        etiqueta.text = "Ya Cambie"
+//        etiqueta.text = "Ya Cambie por segunda vez"
+//        etiqueta.text = "Ya Cambie  por tercera vez"
+//        etiqueta.text = "Ya Cambie por cuarta vez"
+        myButton.setTitle("Mi Boton", for: .normal)
+        myButton.backgroundColor = .blue
+        myButton.setTitleColor(.white, for: .normal)
     }
 
-    @IBAction func cambiarTexto(_ sender: Any) {
-        etiqueta.text = "Hola Mundo Cruel"
-    }
+//    @IBAction func cambiarTexto(_ sender: Any) {
+//        etiqueta.text = "Hola Mundo Cruel"
+//    }
 
+    @IBAction func myButtonAction(_ sender: Any) {
+        if (myButton.backgroundColor == .blue) {
+            myButton.backgroundColor = .green
+        } else {
+            myButton.backgroundColor = .blue
+        }
+    }
 }
 
