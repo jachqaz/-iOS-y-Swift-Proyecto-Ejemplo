788b9c2a587cca15f37ef7aa0193590304d992ce
Johan Castano <jcastano@vovsolutions.com>
2021-08-02 11:55:14 -0500

    ImageViews (Vistas para imágenes)
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
