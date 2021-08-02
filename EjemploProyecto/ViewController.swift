d26495be3c02cc5cfadd7bb51b62c93a2b49a49e
Johan Castano <jcastano@vovsolutions.com>
2021-08-02 14:53:12 -0500

    StackViews (Vistas apiladas horizontal y verticalmente)
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -11,11 +11,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
     @IBOutlet weak var myImage: UIImageView!
+
+    @IBOutlet weak var myFakeView: UIView!
+    @IBOutlet weak var myButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myButtonAction(_ sender: Any) {
+        myFakeView.isHidden = true
+    }
+    
 }
 
