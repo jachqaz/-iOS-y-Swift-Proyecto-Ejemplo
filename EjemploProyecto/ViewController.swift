db9e3ec389bd1b40e0d34ff72cdaa8835b1ad171
Johan Castano <jcastano@vovsolutions.com>
2021-08-17 08:33:31 -0500

    UITableView (Creación de una tabla)
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    private let myCountries = ["España", "Mexico", "Peru", "Colombia", "Argentina", "EEUU"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "mycell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            cell?.backgroundColor = .gray
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        }
+        cell!.textLabel?.text = myCountries[indexPath.row]
+        return cell!
+    }
+
+
+}
