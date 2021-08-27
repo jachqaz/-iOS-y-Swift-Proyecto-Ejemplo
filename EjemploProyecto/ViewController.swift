f12b31523d41b35b7b8e6c0751e747832106650d
Johan Castano <jcastano@vovsolutions.com>
2021-08-27 00:53:48 -0500

    Ciclo de Vida del View Controller
    Signed-off-by: Johan Castano <jcastano@vovsolutions.com>

--- EjemploProyecto/ViewController.swift
+++ EjemploProyecto/ViewController.swift
@@ -11,9 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Estoy en VC1 y se activo -> \(#function)")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Estoy en VC1 y se activo -> \(#function)")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Estoy en VC1 y se activo -> \(#function)")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Estoy en VC1 y se activo -> \(#function)")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Estoy en VC1 y se activo -> \(#function)")
+    }
 }
 
